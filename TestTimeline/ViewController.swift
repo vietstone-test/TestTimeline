fc7237964078eddc79b7890a4592d74798ee097b
vietstone-tech <vietstone.tech@gmail.com>
2018-01-08 14:59:29 +0700

    first commit

---
+++ TestTimeline/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestTimeline
+//
+//  Created by Viet Nguyen Tran on 1/8/18.
+//  Copyright © 2018 Viet Nguyen Tran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
